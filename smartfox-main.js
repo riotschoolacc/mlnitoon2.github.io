108954241ff782e1dd073978c77a82bb39ed92a2
eyesing <77251330+mlnitoon2@users.noreply.github.com>
2025-01-17 21:47:16 -0600

    Beautify

--- smartfox-main.js
+++ smartfox-main.js
@@ -4,58 +4,52 @@
     config.port = 9933;
     config.debug = false;
     config.useSSL = false;
- 
+
     // Create SmartFox client instance
     sfs = new SFS2X.SmartFox(config);
- 
+
     // Set logging
     sfs.logger.level = SFS2X.LogLevel.DEBUG;
     sfs.logger.enableConsoleOutput = true;
     sfs.logger.enableEventDispatching = true;
- 
+
     // Add event listeners
     sfs.addEventListener(SFS2X.SFSEvent.CONNECTION, onConnection, this);
     sfs.addEventListener(SFS2X.SFSEvent.CONNECTION_LOST, onConnectionLost, this);
- 
+
     // Attempt connection
     sfs.connect();
 
-function onConnection(event)
-{
-    if (event.success)
-    {
-        console.log("Connected to SmartFoxServer 2X!<br>SFS2X API version: " + sfs.version);
- 
-        // Show disconnect button
-        switchButtons();
+    function onConnection(event) {
+        if (event.success) {
+            console.log("Connected to SmartFoxServer 2X!<br>SFS2X API version: " + sfs.version);
+
+            // Show disconnect button
+            switchButtons();
+        } else {
+            console.log("Connection failed: " + (event.errorMessage ? event.errorMessage + " (" + event.errorCode + ")" : "Is the server running at all?"));
+
+            // Reset
+            reset();
+        }
     }
-    else
-    {
-        console.log("Connection failed: " + (event.errorMessage ? event.errorMessage + " (" + event.errorCode + ")" : "Is the server running at all?"));
- 
+
+    function onConnectionLost(event) {
+        console.log("Disconnection occurred; reason is: " + event.reason);
         // Reset
         reset();
     }
-}
 
-function onConnectionLost(event)
-{
-    console.log("Disconnection occurred; reason is: " + event.reason);
-    // Reset
-    reset();
-}
- 
-function reset()
-{
- 
-    // Remove SFS2X listeners
-    sfs.removeEventListener(SFS2X.SFSEvent.CONNECTION, onConnection);
-    sfs.removeEventListener(SFS2X.SFSEvent.CONNECTION_LOST, onConnectionLost);
-     
-    sfs.logger.removeEventListener(SFS2X.LoggerEvent.DEBUG, onDebugLogged);
-    sfs.logger.removeEventListener(SFS2X.LoggerEvent.INFO, onInfoLogged);
-    sfs.logger.removeEventListener(SFS2X.LoggerEvent.WARNING, onWarningLogged);
-    sfs.logger.removeEventListener(SFS2X.LoggerEvent.ERROR, onErrorLogged);
-     
-    sfs = null;
-}
+    function reset() {
+
+        // Remove SFS2X listeners
+        sfs.removeEventListener(SFS2X.SFSEvent.CONNECTION, onConnection);
+        sfs.removeEventListener(SFS2X.SFSEvent.CONNECTION_LOST, onConnectionLost);
+
+        sfs.logger.removeEventListener(SFS2X.LoggerEvent.DEBUG, onDebugLogged);
+        sfs.logger.removeEventListener(SFS2X.LoggerEvent.INFO, onInfoLogged);
+        sfs.logger.removeEventListener(SFS2X.LoggerEvent.WARNING, onWarningLogged);
+        sfs.logger.removeEventListener(SFS2X.LoggerEvent.ERROR, onErrorLogged);
+
+        sfs = null;
+    }
