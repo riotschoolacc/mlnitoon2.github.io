6565fcda973075d856231ba771b48e0f045bb478
eyesing <77251330+mlnitoon2@users.noreply.github.com>
2025-01-25 11:21:50 -0600

    FIXC

--- signup.js
+++ signup.js
@@ -1,12 +1,3 @@
-document.getElementById('signup-role').addEventListener('change', function () {
-  const classCodeField = document.getElementById('signup-class');
-  if (this.value === 'student') {
-    classCodeField.classList.remove('hidden');
-  } else {
-    classCodeField.classList.add('hidden');
-  }
-});
-
 document.getElementById('signup-form').addEventListener('submit', function (e) {
   e.preventDefault();
   const username = document.getElementById('signup-username').value;
